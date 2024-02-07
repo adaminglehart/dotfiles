cc2f99aca3630cc99f239bfadc61db4a304eba47
Adam Inglehart <adam.inglehart@gmail.com>
2024-02-07 01:06:07 -0500

    remove ghq

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,15 +1,15 @@
 function fish_user_key_bindings
-  # fzf
-  bind \cf fzf_change_directory
+    # fzf
+    bind \cf fzf_change_directory
 
-  # vim-like
-  # bind \cl forward-char
+    # vim-like
+    # bind \cl forward-char
 
-  # prevent iterm2 from closing when typing Ctrl-D (EOF)
-  bind \cd delete-char
+    # prevent iterm2 from closing when typing Ctrl-D (EOF)
+    bind \cd delete-char
 
-  bind \el _fzf_search_git_log
-  bind \es _fzf_search_git_status
+    bind \el _fzf_search_git_log
+    bind \es _fzf_search_git_status
 end
 
 # fzf plugin
