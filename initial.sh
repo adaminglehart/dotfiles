8aee31255df3668981f861481957cd5dff3d5030
Adam Inglehart <adam.inglehart@gmail.com>
2024-12-26 14:41:01 -0800

    remove zsh configs

--- initial.sh
+++ initial.sh
@@ -6,15 +6,3 @@ if test ! $(which brew); then
     echo "Installing Homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-rm -rf $HOME/.p10k.zsh
-ln -s $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
-
-# install oh-my-zsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
