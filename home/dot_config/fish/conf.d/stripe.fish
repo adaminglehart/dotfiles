7cc1600453e3f8b55b39ea5dedcccbcbc38c3e18
Adam Inglehart <adam@privy.io>
2025-11-12 15:55:42 -0500

    move to chezmoi

--- home/dot_config/fish/conf.d/stripe.fish
+++ home/dot_config/fish/conf.d/stripe.fish
@@ -1,9 +1,9 @@
-if test "$ENVIRONMENT" = stripe
+if test -f "~/stripe/space-commander/bin"
     echo "loading stripe config"
     # Added by `nodenv init` on Thu Sep  4 10:41:35 EDT 2025
     status --is-interactive; and nodenv init - --no-rehash fish | source
 
     nodenv shell 22.18.0
 
-    set -gx PATH ~/stripe/space-commander/bin $PATH
+    fish_add_path ~/stripe/space-commander/bin
 end
