0147561e3d766b5d182080bf63a8e211b98de5bf
Adam Inglehart <adam.inglehart@gmail.com>
2026-03-12 01:36:45 -0400

    templated opencode config

--- home/dot_config/fish/user_conf.d/environment.fish
+++ home/dot_config/fish/user_conf.d/environment.fish
@@ -2,3 +2,7 @@
 if test (which op) && test (which fnox) && test -z "$FNOX_AGE_KEY"
     set -g FNOX_AGE_KEY (op_cached "op://homelab/age key/secretkey")
 end
+
+if test (which op)
+    set -gx HONCHO_API_KEY (op_cached "op://dotenv/HONCHO_API_KEY/value")
+end
